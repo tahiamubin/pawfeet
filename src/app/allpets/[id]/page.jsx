a2549addfead392c846d89e2548514b51a606a21
tahiamubin <tahia.mubin1@gmail.com>
2026-05-20 01:06:59 +0600

    details finished

--- src/app/allpets/[id]/page.jsx
+++ src/app/allpets/[id]/page.jsx
@@ -13,11 +13,39 @@ import { FaGem, FaLocationDot } from "react-icons/fa6";
 import { MdHealthAndSafety } from "react-icons/md";
 import { PiGenderMaleFill } from "react-icons/pi";
 import { TbCoinTakaFilled, TbVaccine } from "react-icons/tb";
+//   "_id": "6a0c0f4a27970a1b25e967e4",
+// "petName": "Bubby",
+// "breed": "Golden Retriever",
+// "species": "Dog",
+// "gender": "Male",
+// "age": "2 years",
+// "health": "good",
+// "vaccine": "vaccinated",
+// "location": "Dhaka",
+// "fee": "150",
+// "imageUrl": "https://cdn.pixabay.com/photo/2018/03/31/06/31/dog-3277416_960_720.jpg",
+// "email": "amin12@gmail.com",
+// "description": "Buddy is a friendly and energet
 
 const PetDetailsPage = async ({ params }) => {
   const { id } = await params;
-  const res = await fetch(`http://localhost:5000/all-pet/${_id}`)
- 
+  const res = await fetch(`http://localhost:5000/all-pet/${id}`);
+  const pet = await res.json();
+  //console.log(pet)
+  const {
+    _id,
+    petName,
+    breed,
+    species,
+    gender,
+    age,
+    health,
+    fee,
+    location,
+    imageUrl,
+    vaccine,
+    description,
+  } = pet;
 
   return (
     <div>
@@ -30,28 +58,28 @@ const PetDetailsPage = async ({ params }) => {
              height={500}>
              </Image> */}
           <div className="card-body">
-            <h2 className="card-title text-xl font-bold">Name</h2>
+            <h2 className="card-title text-xl font-bold">{petName}</h2>
             <div className="text-gray-500 flex ">
-              <p>breed</p>
-              <p className="items-center flex">age</p>
-              <p className="items-center flex">gender</p>
+              <p>{breed}</p>
+              <p className="items-center flex">{age}</p>
+              <p className="items-center flex">{gender}</p>
             </div>
             {/* species and breed */}
             <div className="flex gap-2 ">
               <div className=" bg-[#EBF3EE] shadow-sm p-2 rounded-2xl w-full">
                 <h2 className="text-gray-500">Species</h2>
-                <p className="text-xl font-bold">Dog</p>
+                <p className="text-xl font-bold">{species}</p>
               </div>
               <div className=" bg-[#EBF3EE] shadow-sm p-2 rounded-2xl w-full">
-                <h2 className="text-gray-500">Breed</h2>
-                <p className="text-xl font-bold">Husky</p>
+                <h2 className="text-gray-500">{breed}</h2>
+                <p className="text-xl font-bold">{species}</p>
               </div>
             </div>
             {/* age and gender */}
             <div className="flex gap-2 ">
               <div className=" bg-[#EBF3EE] shadow-sm p-2 rounded-2xl w-full">
                 <h2 className="text-gray-500">Age</h2>
-                <p className="text-xl font-bold">7 months</p>
+                <p className="text-xl font-bold">{age}</p>
               </div>
               <div className=" bg-[#EBF3EE] shadow-sm p-2 rounded-2xl w-full">
                 <h2 className="text-gray-500 flex items-center gap-1">
@@ -60,7 +88,7 @@ const PetDetailsPage = async ({ params }) => {
                 </h2>
                 <p className="text-xl font-bold flex items-center gap-1">
                   <FaGem />
-                  male
+                  {gender}
                 </p>
               </div>
             </div>
@@ -71,14 +99,14 @@ const PetDetailsPage = async ({ params }) => {
                   <FaLocationDot />
                   Location
                 </h2>
-                <p className="text-xl font-bold">Dhaka</p>
+                <p className="text-xl font-bold">{location}</p>
               </div>
               <div className=" bg-[#EBF3EE] shadow-sm p-2 rounded-2xl w-full">
                 <h2 className="text-gray-500 flex items-center gap-1">
                   <TbCoinTakaFilled />
                   Adoption fee
                 </h2>
-                <p className="text-xl font-bold">30000</p>
+                <p className="text-xl font-bold">{fee}</p>
               </div>
             </div>
             {/* health and vaccine */}
@@ -88,14 +116,14 @@ const PetDetailsPage = async ({ params }) => {
                   <MdHealthAndSafety />
                   Health status
                 </h2>
-                <p className="text-xl font-bold">Good</p>
+                <p className="text-xl font-bold">{health}</p>
               </div>
               <div className=" bg-[#EBF3EE] shadow-sm p-2 rounded-2xl w-full">
                 <h2 className="text-gray-500 flex items-center gap-1">
                   <TbVaccine />
                   Vaccine
                 </h2>
-                <p className="text-xl font-bold">vaccinated</p>
+                <p className="text-xl font-bold">{vaccine}</p>
               </div>
             </div>
           </div>
@@ -106,24 +134,21 @@ const PetDetailsPage = async ({ params }) => {
             {/* pet Name */}
             <div className="md:col-span-2">
               <TextField name="petName" isRequired>
-                <Label>Pet Name</Label>
+                <Label>pet Name</Label>
                 <Input
-                  placeholder="petName"
+                  placeholder={petName}
                   className="rounded-2xl bg-gray-100 cursor-default"
                   readOnly
                 />
                 <FieldError />
               </TextField>
             </div>
+            {/* after adding session */}
             {/* your name */}
-           <div className="md:col-span-2">
+            <div className="md:col-span-2">
               <TextField name="Name" isRequired>
                 <Label>Your Name</Label>
-                <Input
-                  placeholder="Name"
-                  className="rounded-2xl bg-gray-100 cursor-default"
-                  readOnly
-                />
+                <Input placeholder="Name" className="rounded-2xl" readOnly />
                 <FieldError />
               </TextField>
             </div>
@@ -151,10 +176,7 @@ const PetDetailsPage = async ({ params }) => {
             <div className="md:col-span-2">
               <TextField name="description" isRequired>
                 <Label>Description</Label>
-                <TextArea
-                  placeholder="Describe ...."
-                  className="rounded-3xl"
-                />
+                <TextArea placeholder="Describe ...." className="rounded-3xl" />
                 <FieldError />
               </TextField>
             </div>
